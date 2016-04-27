91bd0a94699eca459fe2640fa9a3c22d82c63e8b
Krzysztof Duraj <kdur@ericpol.com>
2016-04-27 15:40:03 +0200

    Hello World

---
+++ nbproject/Makefile-Debug.mk
@@ -0,0 +1,92 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=gfortran
+AS=as
+
+# Macros
+CND_PLATFORM=GNU-Linux
+CND_DLIB_EXT=so
+CND_CONF=Debug
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/main.o
+
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=
+CXXFLAGS=
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=/usr/local/lib/libwt.so /usr/local/lib/libwthttp.so /usr/local/lib/libwtdbo.so /usr/local/lib/libwtdbosqlite3.so
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/project-dealicsson
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/project-dealicsson: /usr/local/lib/libwt.so
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/project-dealicsson: /usr/local/lib/libwthttp.so
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/project-dealicsson: /usr/local/lib/libwtdbo.so
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/project-dealicsson: /usr/local/lib/libwtdbosqlite3.so
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/project-dealicsson: ${OBJECTFILES}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/project-dealicsson ${OBJECTFILES} ${LDLIBSOPTIONS}
+
+${OBJECTDIR}/main.o: main.cc 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -std=c++11 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main.o main.cc
+
+# Subprojects
+.build-subprojects:
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/project-dealicsson
+
+# Subprojects
+.clean-subprojects:
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
